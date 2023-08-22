19090174f1da634a93b5c0940978e47906076566
Nishan Pathak <npathak5348@stu.d214.org>
2023-08-22 10:56:14 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("ronaldo sewey")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Fake World Cup")
+        }
+ 
+    }
+}
