413cecb69437bb29ee794683bb684b35aba2a312
Nishan Pathak <npathak5348@stu.d214.org>
2023-08-28 11:13:27 -0500

    hola amigo

---
+++ BensFile.swift
@@ -0,0 +1,20 @@
+//
+//  BensFile.swift
+//  Swift UI Table View
+//
+//  Created by Nishan Pathak on 8/28/23.
+//
+
+import SwiftUI
+
+struct BensFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BensFile_Previews: PreviewProvider {
+    static var previews: some View {
+        BensFile()
+    }
+}
--- ContentView.swift
+++ ContentView.swift
@@ -1,14 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+    
+  let Schedule = ["Physics", "AP Comp", "", "P4", "P5", "P6", "P7", "P8"]
+       
     var body: some View {
-        VStack {
-            Image("ronaldo sewey")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Fake World Cup")
-            Text("Soccer")
+        
+            
+            
+            VStack {
+                Image("ronaldo sewey")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Fake World Cup")
+            }
+     
         }
- 
     }
-}
+
+    
+ 
+ 
+    
+
